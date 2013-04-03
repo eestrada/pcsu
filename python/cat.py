712a513a702392d8d233196d3ff29e4894df3dfb
Ethan Estrada <ethan.estrada@gmail.com>
2013-04-03 14:36:58 -0600

    More or less finished cat. Also, started split
    The -u option of cat might not work exactly as it is supposed to
    yet, but it doesn't break the main functionality either.
    
    On split I am making an iterator class to generate suffix strings.

--- python/cat.py
+++ python/cat.py
@@ -3,19 +3,28 @@ import sys
 import io
 import argparse
 
-_NAME='cat'
-_DESCRIPTION='Concatenate FILE(s), or standard input, to standard output.'
-_EPILOG='With no FILE, or when FILE is -, read standard input.'
-
 def getargs(argv):
-    prsr = argparse.ArgumentParser(prog=_NAME, description=_DESCRIPTION,
-        epilog=_EPILOG, usage='%(prog)s [-u] [file...]')
+    NAME='cat'
+    DESCRIPTION='''The cat utility will read files in sequence and write their
+        contents to the standard output in the same sequence.'''
+    EPILOG=''
+
+    prsr = argparse.ArgumentParser(prog=NAME, description=DESCRIPTION,
+        epilog=EPILOG, usage='%(prog)s [-u] [file...]')
     prsr.add_argument('file', nargs='*',
-        type=argparse.FileType(mode='rb'), help='''File(s) to
-        concatenate to standard output.''')
+        type=argparse.FileType(mode='rb'), help='''A pathname of an input
+        file. If no file operands are specified, the standard input shall
+        be used. If a file is '-' , the cat utility shall read from the
+        standard input at that point in the sequence. The cat utility shall
+        not close and reopen standard input when it is referenced in this way,
+        but shall accept multiple occurrences of '-' as a file operand. The
+        input files can be any file type (i.e. all file types are simply
+        treated as byte sequences).''')
     prsr.add_argument('-u', dest='unbuffered', action='store_true',
         help='''Write bytes from the input file to the standard output
-            without delay as each is read. (i.e. Reading and writing is unbuffered)''')
+            without delay as each is read (i.e. reading and writing is
+            unbuffered). This can be very inefficient and is not generally
+            recommended.''')
     prsr.add_argument('--version', action='version', version='%(prog)s 1.0')
 
     ns = prsr.parse_args(argv)
@@ -23,17 +32,35 @@ def getargs(argv):
 
     return args
 
-def run(argv):
-    args = getargs(argv)
-    sys.stderr.write(str(args))
-    sys.stderr.write('\n')
+def processargs(args):
+    retargs = args
+    files = retargs['file']
+
+    for i in range(len(files)):
+        if files[i].name == '<stdin>':
+            files[i] = retargs['file'][i].buffer
+
+    if retargs['unbuffered']: retargs['bufsize'] = 1
+    else: retargs['bufsize'] = io.DEFAULT_BUFFER_SIZE 
+        
+    return retargs
+
+def catfiles(args):
     l = args['file']
     for f in l:
-        while f.readable():
-            b = f.read(4096)
+        b = True
+        while bool(b):
+            b = f.read(args['bufsize'])
             sys.stdout.buffer.write(b)
-        f.close()
-    
+        if f.name != '<stdin>': f.close()
+        else: f.flush()
+    sys.stdout.buffer.flush()
+
+def run(argv):
+    rawargs = getargs(argv)
+    args = processargs(rawargs)
+    catfiles(args)
+    sys.exit(0)
 
 if __name__ == "__main__":
     run(sys.argv[1:])
--- python/split.py
+++ python/split.py
@@ -1,2 +1,103 @@
 #!/usr/bin/env python3
+import sys
+import io
+import argparse
+import copy
+
+def getargs(argv):
+    NAME='split'
+    DESCRIPTION='''The split utility shall read an input file and write one or more output files. The default size of each output file shall be 1000 lines. The size of the output files can be modified by specification of the -b or -l options. Each output file shall be created with a unique suffix. The suffix shall consist of exactly suffix_length lowercase letters from the POSIX locale. The letters of the suffix shall be used as if they were a base-26 digit system, with the first suffix to be created consisting of all 'a' characters, the second with a 'b' replacing the last 'a' , and so on, until a name of all 'z' characters is created. By default, the names of the output files shall be 'x' , followed by a two-character suffix from the character set as described above, starting with "aa" , "ab" , "ac" , and so on, and continuing until the suffix "zz" , for a maximum of 676 files.
+
+If the number of files required exceeds the maximum allowed by the suffix length provided, such that the last allowable file would be larger than the requested size, the split utility shall fail after creating the last file with a valid suffix; split shall not delete the files it created with valid suffixes. If the file limit is not exceeded, the last file created shall contain the remainder of the input file, and may be smaller than the requested size.'''
+    EPILOG=''
+
+    prsr = argparse.ArgumentParser(prog=NAME, description=DESCRIPTION,
+        usage='''%(prog)s [-l line_count | -b n[k|m]] [-a suffix_length] [file[name]]''',
+        epilog=EPILOG)
+    prsr.add_argument('file', type=argparse.FileType(mode='rt'), default='-',
+        nargs='?', help='''The pathname of the ordinary file to be split. If no
+        input file is given or file is '-' , the standard input shall be
+        used.''')
+    prsr.add_argument('name', type=str, nargs='?', default='x', help='''The
+        prefix to be used for each of the files resulting from the split
+        operation. If no name argument is given, 'x' shall be used as the
+        prefix of the output files. The combined length of the basename of
+        prefix and suffix_length cannot exceed {NAME_MAX} bytes. See the
+        OPTIONS section.''')
+    prsr.add_argument('-a', metavar='suffix_length', default=2,
+        action='store', type=int, help='''Use suffix_length letters to form the
+        suffix portion of the filenames of the split file. If -a is not
+        specified, the default suffix length shall be two. If the sum of the
+        name operand and the suffix_length option-argument would create a
+        filename exceeding {NAME_MAX} bytes, an error shall result; split shall
+        exit with a diagnostic message and no files shall be created.''')
+    prsr.add_argument('-b', metavar ='n', action='store', type=bytecount, 
+        default=None, help='''Split a file into pieces n bytes in size.''')
+    prsr.add_argument('-l', metavar ='line_count', action='store', type=int,
+        default=1000, help='''Specify the number of lines in each resulting
+        file piece. The line_count argument is an unsigned decimal integer.
+        The default is 1000. If the input does not end with a <newline>, the
+        partial line shall be included in the last output file.''')
+    prsr.add_argument('--version', action='version', version='%(prog)s 1.0')
+
+    ns = prsr.parse_args(argv)
+    args = vars(ns)
+
+    return args
+
+def bytecount(s):
+    if s[-1] in 'kK':
+        return int(s[:-1]) * 1024 
+    if s[-1] in 'mM':
+        return int(s[:-1]) * 1024 * 1024
+    else:
+        return int(s)
+
+def processargs(args):
+    retargs = args
+
+    if retargs['b'] != None:
+        retargs['file'] = retargs['file'].buffer
+        
+    return retargs
+
+
+class suffix:
+    '''This is an iterator class'''
+    def __init__(self, count):
+        self.count = count
+        self.max_len = pow(26, count)
+        sys.stderr.write(str(self.max_len) + '\n')
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        raise StopIteration()
+
+def processtext(args):
+    si = suffix(args['a'])
+    raise NotImplementedError()
+    
+    l = True
+    while l:
+        for i in range(args['l']):
+            l = args['file'].readline()
+
+def processbinary(args):
+    raise NotImplementedError()
+
+def splitfile(args):
+    if args['b'] != None: processbinary(args)
+    else: processtext(args)
+
+def run(argv):
+    rawargs = getargs(argv)
+    args = processargs(rawargs)
+    try: splitfile(args)
+    except NotImplementedError: sys.stderr.write(str(args) + '\n')
+    sys.exit(0)
+
+if __name__ == "__main__":
+    run(sys.argv[1:])
 
