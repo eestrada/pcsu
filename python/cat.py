ee6e0b847b1f0f46510a8acd31288c34088cb330
Ethan Estrada <ethan.estrada@gmail.com>
2013-04-03 02:24:35 -0600

    General work on cat.py
    For some reason it won't progress past the first input file. I
    need to figure out what I am doing wrong.

--- python/cat.py
+++ python/cat.py
@@ -3,16 +3,20 @@ import sys
 import io
 import argparse
 
-#print("hola, mundo.")
-
-NAME='cat'
-DESCRIPTION='Concatenate FILE(s), or standard input, to standard output.'
-EPILOG='With no FILE, or when FILE is -, read standard input.'
+_NAME='cat'
+_DESCRIPTION='Concatenate FILE(s), or standard input, to standard output.'
+_EPILOG='With no FILE, or when FILE is -, read standard input.'
 
 def getargs(argv):
-    prsr = argparse.ArgumentParser(prog=NAME, description=DESCRIPTION, epilog=EPILOG)
-    prsr.add_argument('file', metavar='FILE', nargs='*',
-        type=argparse.FileType(mode='rb', bufsize=4096))
+    prsr = argparse.ArgumentParser(prog=_NAME, description=_DESCRIPTION,
+        epilog=_EPILOG, usage='%(prog)s [-u] [file...]')
+    prsr.add_argument('file', nargs='*',
+        type=argparse.FileType(mode='rb'), help='''File(s) to
+        concatenate to standard output.''')
+    prsr.add_argument('-u', dest='unbuffered', action='store_true',
+        help='''Write bytes from the input file to the standard output
+            without delay as each is read. (i.e. Reading and writing is unbuffered)''')
+    prsr.add_argument('--version', action='version', version='%(prog)s 1.0')
 
     ns = prsr.parse_args(argv)
     args = vars(ns)
@@ -21,7 +25,15 @@ def getargs(argv):
 
 def run(argv):
     args = getargs(argv)
-    print(args)
+    sys.stderr.write(str(args))
+    sys.stderr.write('\n')
+    l = args['file']
+    for f in l:
+        while f.readable():
+            b = f.read(4096)
+            sys.stdout.buffer.write(b)
+        f.close()
+    
 
 if __name__ == "__main__":
     run(sys.argv[1:])
