9bec334695d31c37686a4367ab5d0e12ad033327
Ethan D. Estrada <ethan.estrada@gmail.com>
2013-04-15 01:15:23 -0600

    Started work on "logname" and "mkfifo"

--- python/pcsu/logname.py
+++ python/pcsu/logname.py
@@ -1,17 +1,38 @@
 #!/usr/bin/env python3
 import sys
 import os
+import argparse
+
+def getargs(argv):
+    NAME='logname'
+    DESCRIPTION='''The logname utility will write the user's login name to
+    standard output. The login name will be the string that would be returned
+    by the getlogin() function defined in the System Interfaces volume of POSIX
+    .1-2008. Under the conditions where the getlogin() function would fail, the
+    logname utility will write a diagnostic message to standard error and exit
+    with a non-zero exit status.'''
+
+    prsr = argparse.ArgumentParser(prog=NAME, description=DESCRIPTION)
+    prsr.add_argument('--version', action='version',
+        version='%(prog)s (PCSU) 1.0')
+
+    ns = prsr.parse_args(argv)
+    args = vars(ns)
+
+    return args
 
 def run(argv):
-    prog = os.path.basename(argv[0])
-    msg = 'utility "{}" not yet implemented'.format(prog)
+    getargs(argv[1:])
 
     try:
         # Run main functions in here
-        raise NotImplementedError(msg, 1)
+        logname = os.getlogin()
     except Exception as e:
         sys.stderr.write('{}: {}\n'.format(e.__class__.__name__, e.args[0]))
-        raise SystemExit(e.args[1])
+        raise SystemExit(1)
+    else:
+        sys.stdout.write('{}\n'.format(logname))
+        raise SystemExit(0)
 
 def _test(argv):
     '''Run tests on module code'''
--- python/pcsu/mkfifo.py
+++ python/pcsu/mkfifo.py
@@ -1,17 +1,81 @@
 #!/usr/bin/env python3
 import sys
 import os
+import argparse
+
+def getargs(argv):
+    NAME='mkfifo'
+    DESCRIPTION='''\
+    The mkfifo utility will create the FIFO special files specified by the operands, in the order specified.
+
+
+    For each file operand, the mkfifo utility will perform actions equivalent to the mkfifo() function defined in the System Interfaces volume of POSIX.1-2008, called with the following arguments:
+
+        The file operand is used as the path argument.
+
+        The value of the bitwise-inclusive OR of S_IRUSR, S_IWUSR, S_IRGRP, S_IWGRP, S_IROTH, and S_IWOTH is used as the mode argument. (If the -m option is specified, the value of the mkfifo() mode argument is unspecified, but the FIFO will at no time have permissions less restrictive than the -m mode option-argument.)
+'''
+
+    prsr = argparse.ArgumentParser(prog=NAME, description=DESCRIPTION,
+        add_help=False)
+
+    prsr.add_argument('-m', metavar='mode', action='store',
+        default=None, type=pmode)
+        
+    prsr.add_argument('file', nargs='+', action='store',
+        type=pathc)
+
+    prsr.add_argument('--version', action='version',
+        version='%(prog)s (PCSU) 1.0')
+
+    prsr.add_argument('--help', action='help',
+        help='print this help message and exit')
+
+    ns = prsr.parse_args(argv)
+    args = vars(ns)
+
+    return args
+
+def makefifos(args):
+    sys.stderr.write('{}\n'.format(args))
+    raise NotImplementedError('makefifos function not implemented')
+
+def _chmod_mode(l):
+    return l
+    raise NotImplementedError('_chmod_mode function not implemented')
+    return int()
+
+def pathc(s):
+    #raise NotImplementedError('pathc function not implemented')
+    return s + '_the_poop' 
+    
+
+def pmode(s):
+    retval = None
+    try:
+        retval = int(s, 8)
+    except ValueError as ve:
+        try:
+            retval = int(s)
+        except ValueError as ve2:
+            l = list(s)
+            retval = _chmod_mode(l)
+        else:
+            raise ValueError('mode is not an octal integer value')
+
+    return retval
 
 def run(argv):
-    prog = os.path.basename(argv[0])
-    msg = 'utility "{}" not yet implemented'.format(prog)
+    args = getargs(argv[1:])
 
     try:
         # Run main functions in here
-        raise NotImplementedError(msg, 1)
+        makefifos(args)
     except Exception as e:
         sys.stderr.write('{}: {}\n'.format(e.__class__.__name__, e.args[0]))
-        raise SystemExit(e.args[1])
+        raise SystemExit(1)
+    else:
+        raise SystemExit(0)
 
 def _test(argv):
     '''Run tests on module code'''
