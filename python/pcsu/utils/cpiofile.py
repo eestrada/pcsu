f52e53c4376f88309b406459383443d8441e7806
Ethan Estrada <ethan.estrada@gmail.com>
2014-04-25 10:51:32 -0600

    Added more relevant constants to cpiofile.py

--- python/pcsu/utils/cpiofile.py
+++ python/pcsu/utils/cpiofile.py
@@ -7,13 +7,56 @@ import stat
 import time
 import copy
 
-OLD_BINARY_FORMAT = 0
-PORTABLE_ASCII_FORMAT = 1
-NEW_ASCII_FORMAT = 2
-DEFAULT_FORMAT = PORTABLE_ASCII_FORMAT
+from builtins import open as _open # Since 'open' is TarFile.open
+from builtins import type as _type 
 
-ASCII_MAGIC = b'070707' 
-BINARY_MAGIC = oct(ASCII_MAGIC) 
+#######################################################################
+# cpio.h constants
+#######################################################################
+
+# c_mode constants
+C_IRUSR =   int('0000400')
+C_IWUSR =   int('0000200')
+C_IXUSR =   int('0000100')
+
+C_IRGRP =   int('0000040')
+C_IWGRP =   int('0000020')
+C_IXGRP =   int('0000010')
+
+C_IROTH =   int('0000004')
+C_IWOTH =   int('0000002')
+C_IXOTH =   int('0000001')
+
+C_ISUID =   int('0004000')
+C_ISGID =   int('0002000')
+C_ISVTX =   int('0001000')
+
+C_ISDIR =   int('0040000')
+C_ISFIFO =  int('0010000')
+C_ISREG =   int('0100000')
+C_ISBLK =   int('0060000')
+C_ISCHR =   int('0020000')
+C_ISCTG =   int('0110000')
+C_ISLNK =   int('0120000')
+C_ISSOCK =  int('0140000')
+
+MAGIC = b'070707'
+
+# end of cpio.h constants
+
+# other constants
+BINARY_MAGIC = int(MAGIC) 
+POSIX_MAGIC = MAGIC 
+ASCII_MAGIC = b'070701' 
+
+NULL = b'\0'
+BLOCKSIZE = 512
+
+BIN_FORMAT = 0
+POSIX_FORMAT = 1
+ASCII_FORMAT = 2
+DEFAULT_FORMAT = POSIX_FORMAT
+
 
 class CpioError(Exception):
     """Base Exception."""
@@ -92,8 +135,17 @@ class CpioInfo(object):
         raise NotImplementedError()
 
 class CpioFile(object):
-    def __init__(self, name, mode):
+    def __init__(self, name, mode, **kwargs):
         pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        return False
+
+    def open(self, **kwargs):
+        raise NotImplementedError()
 
 def open(name=None, mode='r', fileobj=None, bufsize=10240, **kwargs):
     pass
