b34b8fc3e4c79b305e542a6a80854048fcb6eb23
Ethan Estrada <ethan@misterfidget.com>
2019-06-24 23:07:13 -0600

    Use `write` instead of `display` in tee

--- racket/pcsu/tee.rkt
+++ racket/pcsu/tee.rkt
@@ -5,5 +5,5 @@
 (module+ test)
 
 (module+ main
-  (displayln (find-system-path 'run-file)) ;; name of the file that is executed
-  (displayln (current-command-line-arguments))) ;; remainder of command line args
+  (writeln (find-system-path 'run-file)) ;; name of the file that is executed
+  (writeln (current-command-line-arguments))) ;; remainder of command line args
