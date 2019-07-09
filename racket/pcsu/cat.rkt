06b4410e3b9bee621e769b7d23ddb6ce83793a77
Ethan Estrada <ethan@misterfidget.com>
2019-07-08 23:57:27 -0600

    Attempt different approach to Racket `cat` util

--- racket/pcsu/cat.rkt
+++ racket/pcsu/cat.rkt
@@ -11,13 +11,18 @@
   ;; remainder of command line args
   ;;(writeln (current-command-line-arguments))
   (define arg-list (vector->list (current-command-line-arguments)))
-  (when (equal? (car arg-list) "-u") ;; No buffering is done anyway with copy-port, so this is pointless
+  (when (and (pair? arg-list)
+             (equal? (car arg-list) "-u")) ;; No buffering is done anyway with copy-port, so this is pointless
     (set! arg-list (cdr arg-list)))
-  (define (copy-io)
-    (copy-port (current-input-port) (current-output-port)))
-  (define (write-out-file fp)
-	(if (equal? fp "-")
-	  (copy-io)
-	  (with-input-from-file fp copy-io)))
-  ;; TODO: add code for case of no CLI arguments (i.e. just pipe stdin to stdout)
-  (for-each write-out-file arg-list))
+  (when (null? arg-list) ;; When it is just stdin -> stout
+    (set! arg-list (list "-")))
+
+  (define (open-input fstr)
+    (if (equal? fstr "-")
+      (current-input-port)
+      (open-input-file fstr)))
+  ;; TODO: add code to gracefully deal with non-existent files like `cat` does
+  ;; (i.e. print an error to stderr and just skip the file)
+  (define meta-input-port (apply input-port-append #f (map open-input arg-list)))
+  (copy-port meta-input-port (current-output-port))
+  #|(for-each write-out-file arg-list)|#)
