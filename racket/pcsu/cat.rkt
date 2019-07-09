4e901d62f008c7b05e02eac9ad77f5caaec06bc0
Ethan Estrada <ethan@misterfidget.com>
2019-07-09 00:38:13 -0600

    Skip missing files

--- racket/pcsu/cat.rkt
+++ racket/pcsu/cat.rkt
@@ -2,6 +2,7 @@
 
 (require racket/port)
 (require racket/cmdline)
+(require ffi/unsafe)
 
 (module+ test)
 
@@ -21,8 +22,25 @@
     (if (equal? fstr "-")
       (current-input-port)
       (open-input-file fstr)))
-  ;; TODO: add code to gracefully deal with non-existent files like `cat` does
-  ;; (i.e. print an error to stderr and just skip the file)
-  (define meta-input-port (apply input-port-append #f (map open-input arg-list)))
+
+  ;; TODO: should we somehow signal a failing exit code when this is run? (i.e. non zero)
+  (define (exn-handler exn file)
+    (if (= (car (exn:fail:filesystem:errno-errno exn)) (lookup-errno 'ENOENT))
+      (begin
+        (display file (current-error-port))
+        (displayln ": No such file or directory" (current-error-port)))
+      (begin
+        (display file (current-error-port))
+        (displayln ": unspecified filesystem error occured" (current-error-port)))))
+
+  (define port-list '())
+  (for ((a arg-list))
+    (with-handlers ((exn:fail:filesystem:errno? (lambda (exn) (exn-handler exn a))))
+      (set! port-list (cons (open-input a) port-list))))
+  (set! port-list (reverse port-list))
+  ;; TODO: refactor to print out missing files at the point at which they
+  ;; would be read into the output (like `cat` does). This may mean abandoning
+  ;; the nice `input-port-append` proc we just started using :( .
+  (define meta-input-port (apply input-port-append #f port-list))
   (copy-port meta-input-port (current-output-port))
   #|(for-each write-out-file arg-list)|#)
